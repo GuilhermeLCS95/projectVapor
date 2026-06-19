94cb44846bc3a636807fe7f2029aa9944b594eb6
João Vicente <joaovicenterangel@gmail.com>
2026-06-19 15:53:04 -0300

    fix: merge conflict

--- src/test/java/com/loja_de_jogos/vapor/VaporApplicationTests.java
+++ src/test/java/com/loja_de_jogos/vapor/VaporApplicationTests.java
@@ -1,11 +1,37 @@
 package com.loja_de_jogos.vapor;
 
+import static org.mockito.Mockito.mock;
+
+import com.loja_de_jogos.vapor.repositories.GameRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@EnableAutoConfiguration(
+    exclude = {
+      DataSourceAutoConfiguration.class,
+      HibernateJpaAutoConfiguration.class,
+      DataJpaRepositoriesAutoConfiguration.class
+    })
+@Import(VaporApplicationTests.TestRepositoryConfiguration.class)
 class VaporApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  @TestConfiguration
+  static class TestRepositoryConfiguration {
+
+    @Bean
+    GameRepository gameRepository() {
+      return mock(GameRepository.class);
+    }
+  }
 }
